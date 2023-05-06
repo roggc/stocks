eb42b82a7ff4a38d5d81f5edd11c1ae29abcf620
roggc <roggc9@gmail.com>
2023-05-06 15:03:15 +0200

    fix bugs when no data

--- client/src/ui/utils/get-chart-data-from-candles.js
+++ client/src/ui/utils/get-chart-data-from-candles.js
@@ -4,16 +4,19 @@ export const getChartDataFromCandles = ({ candles, type }) => {
   if (!candles.length) {
     return [];
   }
+  console.log("candles", candles);
   const data = candles.reduce(
     (d, c) => {
-      c[type].forEach((price, i) => {
+      c[type]?.forEach((price, i) => {
         d[i] = { ...d[i], [`${c.symbol}`]: price };
       });
       return d;
     },
-    candles[0][Y_AXIS_DATA_KEY]?.map((v) => ({
-      [Y_AXIS_DATA_KEY]: v,
-    }))
+    candles
+      .find((c) => c[Y_AXIS_DATA_KEY]?.length)
+      ?.[Y_AXIS_DATA_KEY]?.map((v) => ({
+        [Y_AXIS_DATA_KEY]: v,
+      }))
   );
   return data;
 };
