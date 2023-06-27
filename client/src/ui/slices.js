6dc50a7cd5392f6938259d6f194ae89fc4eabc4b
roggc <roggc9@gmail.com>
2023-06-27 15:04:36 +0200

    use rcs@9.0.8

--- client/src/ui/slices.js
+++ client/src/ui/slices.js
@@ -3,10 +3,12 @@ import { MIN } from "ui/constants_/slider";
 import { MAX } from "ui/constants_/slider";
 
 export const { useSlice, Provider } = getHookAndProviderFromSlices({
-  applicationAdapter: { initialArg: {} },
-  from: { initialArg: MIN },
-  priceType: { initialArg: "o" },
-  selectedOptions: { initialArg: [] },
-  stocks: { initialArg: [] },
-  to: { initialArg: MAX },
+  slices: {
+    applicationAdapter: { initialArg: {} },
+    from: { initialArg: MIN },
+    priceType: { initialArg: "o" },
+    selectedOptions: { initialArg: [] },
+    stocks: { initialArg: [] },
+    to: { initialArg: MAX },
+  },
 });
